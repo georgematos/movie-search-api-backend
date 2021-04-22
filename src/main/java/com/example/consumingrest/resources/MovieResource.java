f428f68207523d09f3fbfa42fd786069c4cd8479
George Matos <georgemattos@gmail.com>
2021-04-22 18:37:19 -0300

    fix: fixing cors

--- src/main/java/com/example/consumingrest/resources/MovieResource.java
+++ src/main/java/com/example/consumingrest/resources/MovieResource.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,7 +16,6 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 
 @RestController
 @RequestMapping(value = "/search")
-@CrossOrigin(origins = "http://localhost:3000")
 public class MovieResource {
 
 	
