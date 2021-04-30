e624e068d547bda2f9e95138eb0f3e64478be877
George Matos <georgemattos@gmail.com>
2021-04-30 09:03:01 -0300

    refactor: obsolet tests removed

--- src/test/java/com/example/consumingrest/MovieSearchAPITests.java
+++ src/test/java/com/example/consumingrest/MovieSearchAPITests.java
@@ -12,7 +12,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class MovieSearchAPITests {
 
 	private WebDriver driver;
